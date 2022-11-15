0e105efae497bc126457192e23bac73ff5630994
Jérémy Riverain <jeremy@geekco.fr>
2022-11-15 16:17:35 +0100

    💄 toggle filters on mobile device

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,69 +1,80 @@
 <template>
-  <div class="card">
-    <div class="card-content">
-      <form>
-        <div class="animal-form">
-          <div class="field is-horizontal">
-            <div class="field-label">
-              <label class="label">Filter animals:</label>
-            </div>
-            <div class="field-body">
-              <div class="field is-narrow">
-                <div class="control">
-                  <label class="checkbox ml-3">
-                    <input type="checkbox" v-model="types" value="sea" />
-                    Sea creatures
-                  </label>
-                  <label class="checkbox ml-3">
-                    <input type="checkbox" v-model="types" value="bugs" />
-                    Bugs
-                  </label>
-                  <label class="checkbox ml-3">
-                    <input type="checkbox" v-model="types" value="fish" />
-                    Fishes
-                  </label>
+  <div>
+    <div class="has-text-right mb-2" v-if="showButton">
+      <button class="button" @click="showForm = !showForm">
+        <span class="icon is-small">
+          <i class="fa-solid" :class="showForm ? 'fa-close' : 'fa-filter'"></i>
+        </span>
+        <span>{{ labelButton }}</span>
+      </button>
+    </div>
+
+    <div class="card" v-show="showForm">
+      <div class="card-content">
+        <form>
+          <div class="animal-form">
+            <div class="field is-horizontal">
+              <div class="field-label">
+                <label class="label">Filter animals:</label>
+              </div>
+              <div class="field-body">
+                <div class="field is-narrow">
+                  <div class="control">
+                    <label class="checkbox ml-3">
+                      <input type="checkbox" v-model="types" value="sea" />
+                      Sea creatures
+                    </label>
+                    <label class="checkbox ml-3">
+                      <input type="checkbox" v-model="types" value="bugs" />
+                      Bugs
+                    </label>
+                    <label class="checkbox ml-3">
+                      <input type="checkbox" v-model="types" value="fish" />
+                      Fishes
+                    </label>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="has-text-right ml-3">
-            <button
-              class="button"
-              @click.prevent="ascendingOrder = !ascendingOrder"
-            >
-              <span class="icon">
-                <i v-if="ascendingOrder" class="fa-solid fa-arrow-up-a-z"></i>
-                <i v-else class="fa-solid fa-arrow-down-a-z"></i>
-              </span>
-              <span v-if="ascendingOrder">Sort Z-A</span>
-              <span v-else>Sort A-Z</span>
-            </button>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label" for="search-name">Search</label>
+            <div class="has-text-right ml-3">
+              <button
+                class="button"
+                @click.prevent="ascendingOrder = !ascendingOrder"
+              >
+                <span class="icon">
+                  <i v-if="ascendingOrder" class="fa-solid fa-arrow-up-a-z"></i>
+                  <i v-else class="fa-solid fa-arrow-down-a-z"></i>
+                </span>
+                <span v-if="ascendingOrder">Sort Z-A</span>
+                <span v-else>Sort A-Z</span>
+              </button>
             </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <input
-                    class="input"
-                    placeholder="Name"
-                    id="search-name"
-                    v-model="term"
-                  />
-                </p>
+            <div class="field is-horizontal">
+              <div class="field-label is-normal">
+                <label class="label" for="search-name">Search</label>
+              </div>
+              <div class="field-body">
+                <div class="field">
+                  <p class="control">
+                    <input
+                      class="input"
+                      placeholder="Name"
+                      id="search-name"
+                      v-model="term"
+                    />
+                  </p>
+                </div>
               </div>
             </div>
+            <div class="field">
+              <label class="checkbox ml-3">
+                <input type="checkbox" v-model="onlyAvailable" />
+                Only show available animals
+              </label>
+            </div>
           </div>
-          <div class="field">
-            <label class="checkbox ml-3">
-              <input type="checkbox" v-model="onlyAvailable" />
-              Only show available animals
-            </label>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -71,7 +82,8 @@
 <script setup lang="ts">
 import type { AnimalType } from '@/model'
 import { useStore } from '@/store'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import { useWindowSize } from '@vueuse/core'
 
 const store = useStore()
 
@@ -110,6 +122,15 @@ const onlyAvailable = computed({
     store.onlyAvailableAnimals = newValue
   },
 })
+
+const { width } = useWindowSize()
+const showForm = ref(width.value > 768)
+
+const showButton = computed(() => width.value <= 768)
+
+const labelButton = computed(() =>
+  showForm.value === true ? 'Close filters' : 'Open filters'
+)
 </script>
 
 <style scoped>
